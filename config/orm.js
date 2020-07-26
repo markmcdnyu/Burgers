180bc9da09c8a22bca42ca4afba6ed199a36ec67
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-26 13:17:34 -0500

    Added the required connection from connectionjs and commented example out to follow throuhg the ORM js file

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,11 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
-// Helper function for SQL syntax.
+
+// MVC CAT EXAMPLE FOR var connection below
+// var connection = require("../config/connection.js");
+
+/* // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
@@ -92,3 +96,4 @@ var orm = {
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
+ */
