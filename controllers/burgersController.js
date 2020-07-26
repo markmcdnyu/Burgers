60ea089a0ec1c00ab182c357079059f72b37496c
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-26 18:10:28 -0500

    Added in the POST route

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -6,20 +6,27 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 
-// get route -> index
+// get route
 router.get("/", function (req, res) {
   res.redirect("/burgers");
 });
 
 
 router.get("/burgers", function (req, res) {
-  // express callback response by calling burger.selectAllBurger
   burger.all(function (burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
     res.render("index", { burger_data: burgerData });
   });
 });
 
+
+// post route 
+router.post("/burgers/create", function (req, res) {
+  //Use the req.body.burger_name to create a burger and have access to it 
+  burger.create(req.body.burger_name, function (result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
 
 
 /*
