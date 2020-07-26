1b33489c3cc9487667ad8788dbd59ddac8de2a1e
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-26 17:59:56 -0500

    Syntax fix

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ var routes = require("./controllers/burgersController.js");
 app.use(routes);
 
 app.listen(PORT, function () {
-    console.log("Listening on port:%s", PORT);
+    console.log("Listening on port:", PORT);
 });
