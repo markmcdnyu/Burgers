d890d1ef6d11974db9f93e70958dd1e7704fef35
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-26 13:15:06 -0500

    Inserted the MySQL connection and the setup for a JAWSDB connection in connection js file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
-// Set up MySQL connection.
+// Connection to mysql database and JAWSDB
+
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+}
 
-// Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -18,5 +23,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
