c5af62db40dd0b01c571958dbd2e7858defc0624
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-26 17:59:45 -0500

    Added the server js file express, exphbs, routes, and the port listening

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-//Placeholder
+//Require express
+const express = require("express");
+
+// All consts for the PORT and app for express
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Listening on port:%s", PORT);
+});
