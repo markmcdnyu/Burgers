41eeba24e249b2c7099c5a146204bb22476ff0a2
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-26 23:32:12 -0500

    Removed the delete function in the ORM js, and refactored the syntax

--- config/orm.js
+++ config/orm.js
@@ -17,27 +17,16 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
   var arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
-    var value = ob[key];
-
-    // check to skip hidden properties
-    if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      arr.push(key + "=" + value);
-    }
+    arr.push(key + "=" + ob[key]);
   }
-  // translate array of strings to a single comma-separated string
+
   return arr.toString();
 }
 
-// NEED to add the orm variable that allows for getting all the burgers, updating burgers, and adding burgers
 var orm = {
   all: function (tableInput, cb) {
-    let queryString = "SELECT * FROM " + tableInput + ";";
+    var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
@@ -45,43 +34,42 @@ var orm = {
       cb(result);
     });
   },
-  create: function (table, cols, vals, cb) {
-    let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)})`;
 
+
+  create: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
 
     connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   },
 
+
   update: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
-    let queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition}`;
-
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
     console.log(queryString);
     connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
-
-      cb(result);
-    });
-  },
-  delete: function (table, condition, cb) {
-    let queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
       cb(result);
     });
   }
